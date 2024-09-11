ef51c61a4249cc2efabf02632614dd66a7db4c26
Keshav Kumar <keshav0kumar@hotmail.com>
2024-09-11 12:05:33 +0530

    userLoggedIn variable passed

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,13 @@ const router = new express.Router();
 const { signup, login, logout } = require('../controllers/authController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
-router.get("/signup",(req, res) => {
-   res.render('signup', { title: 'Sign Up' });
+router.get("/signup", (req, res) => {
+   const userLoggedIn = req.cookies.token ? true : false;
+   res.render('signup', { title: 'Sign Up', userLoggedIn });
 });
 router.get('/login', (req, res) => {
-   res.render('login', { title: 'Login' });
+   const userLoggedIn = req.cookies.token ? true : false;
+   res.render('login', { title: 'Login', userLoggedIn  });
 });
 router.post("/signup", signup);
 router.post("/login", login);
