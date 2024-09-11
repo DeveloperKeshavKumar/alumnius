15e415ac997f82683ecba32bbd85c67019ba3606
Keshav Kumar <keshav0kumar@hotmail.com>
2024-09-11 12:06:39 +0530

    index.js updated as per ejs templating

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 const fileUpload = require('express-fileupload');
 const app = express();
 require('dotenv').config();
@@ -7,12 +9,19 @@ const router = require("./routes/index");
 
 const PORT = process.env.PORT || 4040;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.json());
 app.use(fileUpload());
-app.use("/api/v1",router);
+
+app.use("/api/v1", router);
 
 app.get("/", (req, res) => {
-   res.send("Server is spinnning up...")
+   const userLoggedIn = req.cookies.token ? true : false;
+   res.render('index', { title: 'Home',userLoggedIn });
 });
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
